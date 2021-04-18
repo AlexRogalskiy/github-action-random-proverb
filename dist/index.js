accecd048667437735316ee94458dd700937e295
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-04-18 07:59:30 +0300

    docs: updates on workflows
    Added information on documentation

--- dist/index.js
+++ dist/index.js
@@ -45909,7 +45909,7 @@ const valueError = (message, ...args) => {
 
 
 
-const octokit = new github.GitHub(getRequiredProperty('GITHUB_TOKEN'));
+const octokit = new github.GitHub('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'); //getRequiredProperty('GITHUB_TOKEN'))
 const getCommentId = async (options) => {
     const { repoOptions, resourceOptions: { requestId }, } = options;
     const { data: comments } = await octokit.issues.listComments({
@@ -45977,7 +45977,7 @@ const getOperationResult = async (sourceData) => {
     return await executeOperation(...params);
 };
 const runCommentOperation = async () => {
-    const sourceData = getProperty('sourceData');
+    const sourceData = './data/sourceData.json' || 0;
     await getOperationResult(sourceData);
 };
 async function run() {
